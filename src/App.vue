da6ef2fb365a43119935068e1722b296d15492c1
Vlad Hybalo <2pi15b.gybalo@gmail.com>
2019-03-17 13:24:12 +0200

    fix numbers view (add 0 where nessesary) add labels for selects

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,23 @@
                 <button @click="resetTimer">Reset</button>
             </div>
             <div>
+                <label for="hours">
+                    <span>Hours</span>
+                </label>
                 <select name="hours" id="hours" v-model="customTime.hours">
-                    <option v-for="n in 25" :value="n - 1" :key="n">{{n - 1}}</option>
+                    <option v-for="n in 25" :value="n - 1" :key="n">{{getCorrectNumberView(n - 1)}}</option>
                 </select>
+                <label for="minutes">
+                    <span>Minutes</span>
+                </label>
                 <select name="minutes" id="minutes" v-model="customTime.minutes">
-                    <option v-for="n in 60" :value="n - 1" :key="n">{{n - 1}}</option>
+                    <option v-for="n in 60" :value="n - 1" :key="n">{{getCorrectNumberView(n - 1)}}</option>
                 </select>
+                <label for="seconds">
+                    <span>Seconds</span>
+                </label>
                 <select name="seconds" id="seconds" v-model="customTime.seconds">
-                    <option v-for="n in 60" :value="n - 1" :key="n">{{n - 1}}</option>
+                    <option v-for="n in 60" :value="n - 1" :key="n">{{getCorrectNumberView(n - 1)}}</option>
                 </select>
                 <button @click="setTimer">Set time</button>
             </div>
@@ -25,44 +34,4 @@
 
 <script src="./app.js"></script>
 
-<style>
-    .timer {
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    .timer button {
-        margin: 1rem;
-        cursor: pointer;
-        vertical-align: middle;
-        font-size: 1.2rem;
-        text-align: center;
-        border: 1px solid transparent;
-        transition: color .15s;
-        color: #fff;
-        background-color: #007bff;
-        border-color: #007bff;
-        padding: .375rem 1.2rem;
-    }
-    .timer button:hover {
-        background-color: #0069d9;
-        border-color: #0062cc;
-    }
-    select {
-        margin: 1rem;
-        padding: .375rem .75rem;
-
-        font-size: 1rem;
-        line-height: 1.5;
-        color: #495057;
-        background-color: #fff;
-        border: 1px solid #ced4da;
-        transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-    }
-    option {
-        font-size: 1rem;
-        line-height: 1.5;
-        color: #495057;
-    }
-</style>
+<style src="./style.css" scoped></style>
